71f2a50c8c6f250e5385797585b70d0b18f3dc31
mj.shin <128403039+mjshin0425@users.noreply.github.com>
2023-08-03 16:21:28 +0900

    [unrated] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, target) {
-    var answer = 0;
-    if (my_string.includes(target)) {
-        answer = 1 ;
-    } else answer = 0;
-    return answer;
+    return Number(my_string.includes(target))
 }
