87a19c2ef8084625bf03c3a20782911356ea113d
mj.shin <128403039+mjshin0425@users.noreply.github.com>
2023-08-28 16:44:41 +0900

    [level 0] Title: 모스부호 (1), Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -1,12 +1,36 @@
-function solution(letter) {
-    var answer = '';
-    let morse = { 
+//function solution(letter) {
+ //   let answer = '';
+ //   let morse = { 
+ //   '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+ //   '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+ //   '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+ //   '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+ //   '-.--':'y','--..':'z'
+ //   }
+    //모스부호는 공백을 기준으로 나뉘어져 있으므로, 공백을 기준으로 나눈 후 모스부호를 입력
+ //   return answer = letter.split(" ").map((e) => morse[e]);
+//}
+
+const morse = { 
     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
     '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
     '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
     '-.--':'y','--..':'z'
+};
+
+function solution(letter) {
+    let currentMorse = '';
+    let answer = '';
+    for(let i=0; i<letter.length; i++){
+        const item = letter[i];
+        if(item === ' '){
+            answer += morse[currentMorse];
+            currentMorse = '';
+        } else {
+            currentMorse += item;
+        }
     }
-    //모스부호는 공백을 기준으로 나뉘어져 있으므로, 공백을 기준으로 나눈 후 모스부호를 입력
-    return answer = letter.split(" ").map((e) => morse[e]).join('');
+    answer += morse[currentMorse];
+    return answer;
 }
