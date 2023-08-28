4626c27de19a6b9f1e97f23db11160a3a47736e6
mj.shin <128403039+mjshin0425@users.noreply.github.com>
2023-08-28 17:36:39 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -3,8 +3,8 @@ function solution(num_list, n) {
     for(let i=0; i < num_list.length;) {
         let innerArr = [];
         for(let j=0; j< n; j++) {
-           innerArr.push(num_list[i]);
-            i++
+           innerArr.push(num_list[i]); //121212
+            i++    // 여기에 i++을 쓰지 않으면 121212 반복. 배열에 들어가는 값이 증가되지 않음
         }
         answer.push(innerArr);
     }
