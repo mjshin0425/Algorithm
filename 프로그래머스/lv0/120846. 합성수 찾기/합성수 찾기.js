4fb776c8de5ca4f70feb41248b0df81a4caa8520
mj.shin <128403039+mjshin0425@users.noreply.github.com>
2023-08-28 16:26:16 +0900

    [level 0] Title: 합성수 찾기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,12 +1,15 @@
 function solution(n) {
     let answer = 0
+    //n 이하를 돌리는 for문
     for(i=1; i<=n; i++) {
         let counter = 0
+        // 합성수를 찾는 for문
         for(j=1; j<=i ; j++) {
             if(i%j == 0) {
                 counter += 1
             }
         }
+//모든 수는 1과 자기 자신을 약수를 가지므로 counter는 3개부터 센다
         if(counter > 2) {
             answer += 1
         }
